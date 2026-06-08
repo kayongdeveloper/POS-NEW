452911d32494a56870bdb03e618fc945b35ad69a
ap-pamungkas <agustinusputra94@gmail.com>
2026-06-08 22:01:01 +0700

    feat(auth):create function logout

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -34,3 +34,4 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     next();
   });
 };
+
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -10,5 +10,6 @@ const authController = new AuthController();
 router.post("/login", authController.login);
 router.post("/refresh", authController.refresh);
 router.get("/me", authenticateToken, authController.getMe);
+router.post("/logout", authenticateToken, authController.logout);
 
 export default router;
